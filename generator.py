d5f4e98f91f60a8fbd61973399d410bbf88d1a1f
kedziorno <kjoblak85@gmail.com>
2025-01-28 14:24:50 +0100

    add_vertical_whitespace

--- generator.py
+++ generator.py
@@ -157,25 +157,35 @@ def add_vertical_whitespace(tokens: List[List[Tuple[str, str]]]) -> List[List[Tu
             (subtokens[1][0] == 'COMMENT' or subtokens[0][0] == 'COMMENT') and 
             len(tokens[i - 1]) > 1 and
             tokens[i - 1][0][0] == 'INSTRUCTION'):
-            new_tokens.append([('WHITESPACE', '')])
+            print #new_tokens.append([('WHITESPACE', '')])
         new_tokens.append(subtokens)
-        print (f"add_vertical_whitespace111 - {len(new_tokens[i])} - {new_tokens[i]}")    
+        print (f"add_vertical_whitespace111 - {i} - {len(new_tokens[i])} - {new_tokens[i]}")    
 
         for subtokens in new_tokens:
             if len(subtokens) < 2 or not (subtokens[2][0] == 'INSTRUCTION'):
                 continue
-            print ("ll "+subtokens[2][1])
+            #print ("ll "+subtokens[2][1])
             longest_len = max(longest_len, len(subtokens[2][1]))
 
         multiplier = 0
         while longest_len > tab_len * multiplier:
             multiplier += 1
 
-        
+
         if (i > 0 and 
             len(subtokens) > 1 and
             len(tokens[i - 1]) > 1 and
-            ((tokens[i - 1][2][0] == 'INSTRUCTION') or (tokens[i - 1][2][0] == 'LABEL_DEFINITION'))):
+            ((tokens[i-1][1][0] == 'LABEL_DEFINITION'))):
+            print ("subtoken333 " + tokens[i-1][2][1])
+    
+            #new_tokens[i].pop(2)
+            new_tokens[i].insert(3, ('WHITESPACE', '|' * ((multiplier+1) * tab_len - len(tokens[i][2][1]))))
+            print (f"add_vertical_whitespace444 - {len(new_tokens[i])} - {new_tokens[i]}")    
+        
+        if (i > 0 and 
+            len(subtokens) > 1 and
+            len(tokens[i - 2]) > 1 and len(tokens[i - 1]) > 1 and
+            ((tokens[i - 1][2][0] == 'INSTRUCTION') or (tokens[i - 2][1][0] == 'LABEL_DEFINITION'))):
             print ("subtoken " + tokens[i][2][1])
     
             #new_tokens[i].pop(2)
